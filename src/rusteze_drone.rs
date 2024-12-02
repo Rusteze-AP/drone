7736b74701939ad1e3cca90dbfabea0c6c40b2b0
enrico-car <car.enrico19@gmail.com>
2024-12-02 17:18:16 +0100

    logger module

--- src/rusteze_drone.rs
+++ src/rusteze_drone.rs
@@ -1,6 +1,7 @@
 use crate::log_debug;
 use crate::messages::{RustezePacket, RustezeSourceRoutingHeader};
 use crossbeam::channel::{select, Receiver, Sender};
+use logger::Logger;
 use rand::Rng;
 use std::collections::{HashMap, HashSet};
 use wg_internal::controller::{DroneCommand, NodeEvent};
@@ -21,6 +22,7 @@ pub struct RustezeDrone {
 
     // Flood related
     flood_history: HashSet<u64>,
+    logger: Logger,
 }
 
 impl Drone for RustezeDrone {
@@ -34,6 +36,7 @@ impl Drone for RustezeDrone {
             controller_recv: options.controller_recv,
             terminated: false,
             flood_history: HashSet::new(),
+            logger: Logger::new(true),
         }
     }
 
@@ -48,24 +51,37 @@ impl RustezeDrone {
         self.id
     }
 
+    pub fn set_display_log(&mut self, display_log: bool) {
+        self.logger.set_displayable(display_log);
+    }
+
     fn packet_dispatcher(&mut self, mut packet: Packet) {
         match packet.pack_type {
             PacketType::MsgFragment(fragment) => {
-                log_debug!(
-                    "[DRONE-{}][🟢 FRAGMENT] - Received a fragment from NODE {}",
-                    self.id,
-                    packet.routing_header.get_previous_hop().unwrap_or(0)
+                self.logger.log_debug(
+                    format!(
+                        "<DRONE-{}><FRAGMENT> - Received a fragment from NODE {}",
+                        self.id,
+                        packet.routing_header.get_previous_hop().unwrap_or(0)
+                    )
+                    .as_str(),
                 );
                 self.fragment_handler(fragment, packet.routing_header, packet.session_id);
             }
             PacketType::Nack(nack) => {
-                log_debug!(
-                    "[DRONE-{}][🟢 NACK] - Received a nack from NODE {}",
-                    self.id,
-                    packet.routing_header.get_previous_hop().unwrap_or(0)
+                self.logger.log_debug(
+                    format!(
+                        "<DRONE-{}><NACK> - Received a nack from NODE {}",
+                        self.id,
+                        packet.routing_header.get_previous_hop().unwrap_or(0)
+                    )
+                    .as_str(),
                 );
+
                 match packet.routing_header.get_current_hop() {
-                    None => log_debug!("[DRONE-{}][🔴 NACK] - No current hop found", self.id),
+                    None => self.logger.log_error(
+                        format!("<DRONE-{}><NACK> - No current hop found", self.id).as_str(),
+                    ),
                     Some(current_node) => {
                         if current_node == self.id {
                             packet.routing_header.increment_index();
@@ -76,19 +92,26 @@ impl RustezeDrone {
                                 packet.routing_header,
                             );
                         } else {
-                            log_debug!("[DRONE-{}][🔴 NACK] - Nack received by the wrong node. Found DRONE {} at current hop. Ignoring!", self.id, current_node);
+                            self.logger.log_error(
+                                format!("<DRONE-{}><NACK> - Nack received by the wrong node. Found DRONE {} at current hop. Ignoring!", self.id, current_node).as_str()
+                            );
                         }
                     }
                 }
             }
             PacketType::Ack(ack) => {
-                log_debug!(
-                    "[DRONE-{}][🟢 ACK] - Received an ack from NODE {}",
-                    self.id,
-                    packet.routing_header.get_previous_hop().unwrap_or(0)
+                self.logger.log_debug(
+                    format!(
+                        "<DRONE-{}><NACK> - Received an ack from NODE {}",
+                        self.id,
+                        packet.routing_header.get_previous_hop().unwrap_or(0)
+                    )
+                    .as_str(),
                 );
                 match packet.routing_header.get_current_hop() {
-                    None => log_debug!("[DRONE-{}][🔴 ACK] - No current hop found", self.id),
+                    None => self.logger.log_error(
+                        format!("<DRONE-{}><NACK> - No current hop found", self.id).as_str(),
+                    ),
                     Some(current_node) => {
                         if current_node == self.id {
                             packet.routing_header.increment_index();
@@ -98,7 +121,9 @@ impl RustezeDrone {
                                 packet.routing_header,
                             );
                         } else {
-                            log_debug!("[DRONE-{}][🔴 ACK] - Ack received by the wrong Node. Found DRONE {} at current hop. Ignoring!", self.id, current_node);
+                            self.logger.log_error(
+                                format!("<DRONE-{}><NACK> - Ack received by the wrong Node. Found DRONE {} at current hop. Ignoring!", self.id, current_node).as_str(),
+                            );
                         }
                     }
                 }
@@ -107,11 +132,11 @@ impl RustezeDrone {
                 self.flood_req_handler(flood_req);
             }
             PacketType::FloodResponse(flood_res) => {
-                log_debug!("Drone {} received flood response {:?}", self.id, flood_res);
+                // log_debug!("Drone {} received flood response {:?}", self.id, flood_res);
                 //TODO make fragment handler generic
             }
             _ => {
-                log_debug!("Drone {} received unknown packet {:?}", self.id, packet);
+                // log_debug!("Drone {} received unknown packet {:?}", self.id, packet);
             }
         }
     }
@@ -128,7 +153,13 @@ impl RustezeDrone {
                     source_routing_header.increment_index();
                     match source_routing_header.get_current_hop() {
                         None => {
-                            log_debug!("Drone {} received fragment and it is at the edge", self.id);
+                            self.logger.log_error(
+                                format!(
+                                    "<DRONE-{}><NACK> - received fragment and it is at the edge",
+                                    self.id,
+                                )
+                                .as_str(),
+                            );
                             self.send_nack(
                                 NackType::DestinationIsDrone,
                                 fragment.fragment_index,
@@ -138,7 +169,13 @@ impl RustezeDrone {
                         }
                         Some(next_node) => match self.packet_senders.get(&next_node) {
                             None => {
-                                log_debug!("Drone {} received fragment and can't forward", self.id);
+                                self.logger.log_error(
+                                    format!(
+                                        "<DRONE-{}><NACK> -received fragment and can't forward",
+                                        self.id,
+                                    )
+                                    .as_str(),
+                                );
                                 self.send_nack(
                                     NackType::ErrorInRouting(next_node),
                                     fragment.fragment_index,
@@ -192,7 +229,13 @@ impl RustezeDrone {
                         },
                     }
                 } else {
-                    log_debug!("Drone {} is not the right fragment receiver", self.id);
+                    self.logger.log_error(
+                        format!(
+                            "<DRONE-{}><NACK> - is not the right fragment receiver",
+                            self.id,
+                        )
+                        .as_str(),
+                    );
                     source_routing_header.increment_index();
                     self.send_nack(
                         NackType::UnexpectedRecipient(self.id),
@@ -203,7 +246,13 @@ impl RustezeDrone {
                 }
             }
             None => {
-                log_debug!("Drone {} is not the right fragment receiver", self.id);
+                self.logger.log_error(
+                    format!(
+                        "<DRONE-{}><NACK> - is not the right fragment receiver",
+                        self.id,
+                    )
+                    .as_str(),
+                );
                 source_routing_header.increment_index();
                 self.send_nack(
                     NackType::UnexpectedRecipient(self.id),
@@ -219,15 +268,15 @@ impl RustezeDrone {
         match command {
             DroneCommand::AddSender(id, sender) => {
                 self.packet_senders.insert(id, sender);
-                log_debug!("Added new sender for node {}", id);
+                // log_debug!("Added new sender for node {}", id);
             }
             DroneCommand::SetPacketDropRate(pdr) => {
                 self.pdr = pdr;
-                log_debug!("Packet drop rate of node {} changed to {}", self.id, pdr);
+                // log_debug!("Packet drop rate of node {} changed to {}", self.id, pdr);
             }
             DroneCommand::Crash => {
                 // exit the thread
-                log_debug!("Received crash command for node {}", self.id);
+                // log_debug!("Received crash command for node {}", self.id);
                 self.terminated = true;
                 // TODO Decide how to handle the crash (packets still in channel?)
             }
@@ -246,31 +295,43 @@ impl RustezeDrone {
             nack_type,
         };
         match source_routing_header.get_current_hop() {
-            None => log_debug!(
-                "[🔴 NACK] - No next hop found. Hops {:?} with hop_index {}",
-                source_routing_header.hops,
-                source_routing_header.hop_index
-            ),
-            Some(previous_node) => match self.packet_senders.get(&previous_node) {
-                None => log_debug!(
-                    "[🔴 NACK] - No match of NODE {} found inside neighbours {:?} at hop_index {}",
-                    previous_node,
+            None => self.logger.log_error(
+                format!(
+                    "<DRONE-{}><NACK> - No next hop found. Hops {:?} with hop_index {}",
+                    self.id,
                     source_routing_header.hops,
                     source_routing_header.hop_index
+                )
+                .as_str(),
+            ),
+            Some(previous_node) => match self.packet_senders.get(&previous_node) {
+                None => self.logger.log_error(
+                    format!(
+                        "<DRONE-{}><NACK> - No match of NODE {} found inside neighbours {:?} at hop_index {}",
+                        self.id,
+                        previous_node,
+                        source_routing_header.hops,
+                        source_routing_header.hop_index
+                    )
+                    .as_str(),
                 ),
                 Some(sender) => {
                     let packet =
                         Packet::new(PacketType::Nack(nack), source_routing_header, session_id);
-                    log_debug!(
-                        "[DRONE-{}][🟢 NACK] - Ack forwarded to NODE {}",
-                        self.id,
-                        previous_node
+                    self.logger.log_debug(
+                        format!(
+                            "<DRONE-{}><NACK> - NAck forwarded to NODE {}",
+                            self.id,
+                            previous_node,
+                        )
+                        .as_str(),
                     );
                     let p1 = packet.clone();
                     sender.send(packet).unwrap();
-                    match (nack_type){
+                    match nack_type {
                         NackType::Dropped => {
-                            self.forward_to_sm_packetDropped(p1);}
+                            self.forward_to_sm_packetDropped(p1);
+                        }
                         _ => self.forward_to_sm_packetSent(p1),
                     }
                 }
@@ -285,17 +346,25 @@ impl RustezeDrone {
         source_routing_header: SourceRoutingHeader,
     ) {
         match source_routing_header.get_current_hop() {
-            None => log_debug!(
-                "[🔴 ACK] - No next hop found. Hops {:?} with hop_index {}",
-                source_routing_header.hops,
-                source_routing_header.hop_index
-            ),
-            Some(previous_node) => match self.packet_senders.get(&previous_node) {
-                None => log_debug!(
-                    "[🔴 ACK] - No match of NODE {} found inside neighbours {:?} at hop_index {}",
-                    previous_node,
+            None => self.logger.log_error(
+                format!(
+                    "<DRONE-{}><ACK> - No next hop found. Hops {:?} with hop_index {}",
+                    self.id,
                     source_routing_header.hops,
                     source_routing_header.hop_index
+                )
+                .as_str(),
+            ),
+            Some(previous_node) => match self.packet_senders.get(&previous_node) {
+                None => self.logger.log_error(
+                    format!(
+                        "<DRONE-{}><ACK> - No match of NODE {} found inside neighbours {:?} at hop_index {}",
+                        self.id,
+                        previous_node,
+                        source_routing_header.hops,
+                        source_routing_header.hop_index
+                    )
+                    .as_str(),
                 ),
                 Some(sender) => {
                     let packet = Packet::new(
@@ -303,10 +372,13 @@ impl RustezeDrone {
                         source_routing_header,
                         session_id,
                     );
-                    log_debug!(
-                        "[DRONE-{}][🟢 ACK] - Ack forwarded to NODE {}",
-                        self.id,
-                        previous_node
+                    self.logger.log_debug(
+                        format!(
+                            "<DRONE-{}><ACK> - Ack forwarded to NODE {}",
+                            self.id,
+                            previous_node,
+                        )
+                        .as_str(),
                     );
                     let p1 = packet.clone();
                     sender.send(packet).unwrap();
@@ -325,7 +397,7 @@ impl RustezeDrone {
     fn internal_run(&mut self) {
         loop {
             if self.terminated {
-                log_debug!("Drone {} terminated", self.id);
+                self.logger.log_info(format!("<Drone-{}><RUNNER> - terminated", self.id).as_str());
                 break;
             }
             select! {
@@ -333,17 +405,17 @@ impl RustezeDrone {
                     match msg {
                         Ok(msg) => self.packet_dispatcher(msg),
                         Err(_) => {
-                            log_debug!("Drone {} receiver disconnected", self.id);
+                            self.logger.log_error(format!("<Drone-{}><RUNNER> - receiver disconnected", self.id).as_str());
                             break;
                         }
                     }
                 }
                 recv(self.controller_recv) -> command => {
                     if let Ok(command) = command {
-                        log_debug!("Drone {} received message from controller", self.id);
+                        self.logger.log_info(format!("<Drone-{}><RUNNER> - received message from controller", self.id).as_str());
                         self.execute_command(command);
                     } else {
-                        log_debug!("Drone {} controller receiver disconnected from Simulation Controller", self.id);
+                        self.logger.log_error(format!("<Drone-{}><RUNNER> - controller receiver disconnected from Simulation Controller", self.id).as_str());
                         break;
                     }
                 }
