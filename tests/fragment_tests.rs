0c8cf547b0195d56d391399aaa007b4fb6bebce2
luiss07 <luigi.delleva@gmail.com>
2024-12-08 21:28:18 +0100

    added packet tests

--- tests/fragment_tests.rs
+++ tests/fragment_tests.rs
@@ -23,3 +23,18 @@ fn drone_chain_fragment_drop() {
 fn drone_chain_fragment_ack() {
     generic_chain_fragment_ack::<RustezeDrone>();
 }
+
+#[test]
+fn ack_forward() {
+    generic_ack_forward::<RustezeDrone>();
+}
+
+#[test]
+fn nack_forward() {
+    generic_nack_forward::<RustezeDrone>();
+}
+
+#[test]
+fn destination_is_drone() {
+    generic_destination_is_drone::<RustezeDrone>();
+}
